147789342c2c1e6c7808ec1fdc941e065d430836
toomv134 <tjdfhr000531@naver.com>
2023-10-28 17:37:23 +0900

    Death Animation
    add Death Animation

--- Assets/Script/Unit/Active/Life.cs
+++ Assets/Script/Unit/Active/Life.cs
@@ -7,15 +7,24 @@ public class Life : MonoBehaviour
 {
     //public static Life instance;
     public float amount;
-    public UnityEvent onDeath;
+    //public UnityEvent onDeath;
 
     private void Update()
     {
         //Debug.Log(amount);
         if (amount <= 0)
         {
-            onDeath.Invoke();
-            Destroy(gameObject);
+            Debug.Log("death");
+            this.GetComponent<Animator>().SetTrigger("IsDeath");
+            StartCoroutine(Death());
         }
+    }
+
+    IEnumerator Death()
+    {
+        
+        //Debug.Log("death");
+        yield return new WaitForSecondsRealtime(2.0f);
+        Destroy(gameObject);
     }
 }
