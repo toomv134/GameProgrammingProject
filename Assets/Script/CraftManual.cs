e8bce0411960ce7c8d9b01c33ac76c1806e9556a
seonwook0215 <58116065+seonwook0215@users.noreply.github.com>
2023-10-30 01:54:03 +0900

    map change

--- Assets/Script/CraftManual.cs
+++ Assets/Script/CraftManual.cs
@@ -75,7 +75,7 @@ public class CraftManual : MonoBehaviour
     {
       
         //GetMouseCursorpos();
-        if (movingCamera.enabled)
+        if (movingCamera.enabled||TurnManager.instance.StartWar)
         {
             Cancel();
         }
